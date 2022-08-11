fb7ac707a9eeb6bb709dfd87e21dba4b9d68a0f4
rubenkristian <rubenkristian96@gmail.com>
2022-08-11 22:46:50 +0700

    add unpin file

--- pinata/remove.go
+++ pinata/remove.go
@@ -1,5 +1,35 @@
 package pinata
 
-func removeFile() {
+import (
+	"io/ioutil"
+	"net/http"
+)
 
+func removeFile(auth string) ([]byte, error) {
+	var result []byte = nil
+	clientRequest := &http.Client{}
+
+	req, errReq := http.NewRequest(string(DELETE), string(UNPIN), nil)
+
+	if errReq != nil {
+		return nil, errReq
+	}
+
+	req.Header.Add("Authorization", auth)
+
+	res, errRes := clientRequest.Do(req)
+
+	if errRes != nil {
+		return nil, errRes
+	}
+
+	defer res.Body.Close()
+
+	result, errReadBody := ioutil.ReadAll(res.Body)
+
+	if errReadBody != nil {
+		return nil, errReadBody
+	}
+
+	return result, nil
 }
